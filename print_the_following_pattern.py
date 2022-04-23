badcbf27b77a0dbe2226702504713351b5cc05c9
thisisvarun007 <98144900+thisisvarun007@users.noreply.github.com>
2022-04-23 11:13:30 +0530

    a=int(input()) for i in range(a):     for j in range(a):         if i==j or j==0 or j==a-1:             print("*",end=" ")         else:             print(" ",end=" ")     print(""),

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,8 @@
 a=int(input())
 for i in range(a):
-    for j in range(a,0,-1):
-        print(j,end=" ")
+    for j in range(a):
+        if i==j or j==0 or j==a-1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
     print(""),

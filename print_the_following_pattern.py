b4b952835ed39852b01a9e79f8a014dea998b751
thisisvarun007 <98144900+thisisvarun007@users.noreply.github.com>
2022-05-19 17:12:45 +0530

    a=int(input()) for i in range(a):     for j in range(1,a-1):         print(j,end="")     for j in range(a-3,0,-1):         print(j,end="")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -2,6 +2,6 @@ a=int(input())
 for i in range(a):
     for j in range(1,a-1):
         print(j,end="")
-    for j in range(1,a-2):
+    for j in range(a-3,0,-1):
         print(j,end="")
     print()

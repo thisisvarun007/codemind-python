196eb2125fa35dccf269a55ed5f866112d902fd6
thisisvarun007 <98144900+thisisvarun007@users.noreply.github.com>
2022-06-01 16:59:34 +0530

    t=int(input()) for k in range(t):     a=int(input())     arr=list(map(int,input().split()))     f=0     for i in range(a):         ls=sum(arr[:i])         rs=sum(arr[i+1:])         if ls==rs:             f=1             break     if ls==rs:         print("YES")     else:         print("NO")

---
+++ Balanced_Array.py
@@ -0,0 +1,15 @@
+t=int(input())
+for k in range(t):
+    a=int(input())
+    arr=list(map(int,input().split()))
+    f=0
+    for i in range(a):
+        ls=sum(arr[:i])
+        rs=sum(arr[i+1:])
+        if ls==rs:
+            f=1
+            break
+    if ls==rs:
+        print("YES")
+    else:
+        print("NO")
